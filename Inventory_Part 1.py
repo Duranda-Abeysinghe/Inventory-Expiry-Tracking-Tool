cab2b79e450a2c5c67f4dc68f6eb463c9d8f867a
Isura-Punsara <isurapunsara27@gmail.com>
2025-08-30 00:02:13 +0530

    Update Inventory_Part 1.py

--- Inventory_Part 1.py
+++ Inventory_Part 1.py
@@ -1,20 +1,20 @@
 class InventoryExpiryTracker:
     def _init_(self):
-        self.expiry_heap = []  # Min Heap for expiry
-        self.add_order_list = []  # Keep track of order added
-        self.stock_queue = deque()  # FIFO log
-        self.threshold = 3  # days for alert
-        self.load_inventory()  # Load saved inventory if exists
-        self.show_alerts()    # Simple reminder on start
+        self.expiry_heap = []  
+        self.add_order_list = [] 
+        self.stock_queue = deque()  
+        self.threshold = 3  
+        self.load_inventory() 
+        self.show_alerts()   
 
-    # Add product
+ 
     def add_product(self, product):
         heapq.heappush(self.expiry_heap, product)
-        self.add_order_list.append(product)  # track insertion order
+        self.add_order_list.append(product)  
         self.stock_queue.append(f"Added: {product.name}")
         self.save_inventory()
 
-    # Remove next expiring product
+   
     def remove_product(self):
         if self.expiry_heap:
             removed = heapq.heappop(self.expiry_heap)
@@ -25,7 +25,7 @@ class InventoryExpiryTracker:
             return removed
         return None
 
-    # Update product quantity
+
     def update_product(self, name, new_qty):
         for prod in self.expiry_heap:
             if prod.name == name:
