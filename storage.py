d423c9347a6778694dccd301623015da86186554
Bimsara Randunu <bimsararandunu9899@gmail.com>
2025-08-30 15:06:02 +0530

    Update storage.py
    Implement inventory save and load functionality with JSON

--- storage.py
+++ storage.py
@@ -1,4 +1,5 @@
-import json
+import json, os
+from product import Product
 
 def save_inventory(add_order_list, filename="inventory.json"):
     data = []
@@ -10,3 +11,14 @@ def save_inventory(add_order_list, filename="inventory.json"):
         })
     with open(filename, "w") as f:
         json.dump(data, f)
+
+def load_inventory(expiry_heap, add_order_list, filename="inventory.json"):
+    if os.path.exists(filename):
+        import heapq
+        with open(filename, "r") as f:
+            data = json.load(f)
+            for item in data:
+                prod = Product(item["name"], item["quantity"], item["expiry_date"])
+                expiry_heap.append(prod)
+                add_order_list.append(prod)
+        heapq.heapify(expiry_heap)
