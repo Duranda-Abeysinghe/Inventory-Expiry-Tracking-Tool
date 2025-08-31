0f991e37b3cfd820637cf7feec48ca8a4a59d7f3
Isura-Punsara <isurapunsara27@gmail.com>
2025-08-31 13:26:04 +0530

    Update Inventory_Part 1.py

--- Inventory_Part 1.py
+++ Inventory_Part 1.py
@@ -1,31 +1,39 @@
 class InventoryExpiryTracker:
     def _init_(self):
-        self.expiry_heap = []  
-        self.add_order_list = [] 
-        self.stock_queue = deque()  
-        self.threshold = 3  
-        self.load_inventory() 
-        self.show_alerts()   
+        self.expiry_heap = []      
+        self.add_order_list = []  
+        self.stock_queue = deque() 
+        self.expired_log = {}      
+        self.threshold = 3         
+        self.load_inventory()      
+        self.show_alerts()         
 
- 
+
     def add_product(self, product):
         heapq.heappush(self.expiry_heap, product)
-        self.add_order_list.append(product)  
-        self.stock_queue.append(f"Added: {product.name}")
+        self.add_order_list.append(product)
+        self.stock_queue.append(f"🔥 Added: {product.name}")  
         self.save_inventory()
 
-   
+    
     def remove_product(self):
         if self.expiry_heap:
             removed = heapq.heappop(self.expiry_heap)
             if removed in self.add_order_list:
                 self.add_order_list.remove(removed)
-            self.stock_queue.append(f"Removed: {removed.name}")
+            
+       
+            if removed.days_until_expiry() < 0:
+                if removed.name not in self.expired_log:
+                    self.expired_log[removed.name] = 0
+                self.expired_log[removed.name] += removed.quantity
+
+            self.stock_queue.append(f"❌ Removed: {removed.name}")  
             self.save_inventory()
             return removed
         return None
 
-
+   
     def update_product(self, name, new_qty):
         for prod in self.expiry_heap:
             if prod.name == name:
@@ -34,4 +42,4 @@ class InventoryExpiryTracker:
                 heapq.heapify(self.expiry_heap)
                 self.save_inventory()
                 return prod
-        return None
+        return None
